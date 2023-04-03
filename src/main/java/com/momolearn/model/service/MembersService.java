9f87342acfe198608bd3d22b764114dcd3d87ddd
Sihyeon Lee <ls477656@gmail.com>
2023-04-03 09:26:58 +0900

    🍋 강사 신청서 등급 변경 기능 수정

--- src/main/java/com/momolearn/model/service/MembersService.java
+++ src/main/java/com/momolearn/model/service/MembersService.java
@@ -183,10 +183,11 @@ public class MembersService {
 	}
 
 	// 회원 등급 변경 : ApplyTeacher에서 사용
-	public Members updateGrade(String membersMemId) throws NotExistException{
+	public MembersDTO updateGrade(String membersMemId) throws NotExistException{
 		Members member = membersRepository.findById(membersMemId).orElseThrow(()->new NotExistException("회원정보가 존재하지 않습니다."));
 		member.setGrade("teacher");
-		return membersRepository.save(member);	
+		membersRepository.save(member);
+		return mapper.map(member, MembersDTO.class);
 	}
 	
 }
